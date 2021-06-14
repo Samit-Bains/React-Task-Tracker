3c3d37f098c46460885131157306abcfecdf0128
samit-bains <samitpalbains@gmail.com>
2021-06-14 13:45:43 -0700

    Initial commit

---
+++ src/components/Header.js
@@ -0,0 +1,11 @@
+//Use rafce to auto-generate template for component
+
+const Header = () => {
+    return (
+        <header>
+            <h1>Task Tracker</h1>
+        </header>
+    )
+}
+
+export default Header
